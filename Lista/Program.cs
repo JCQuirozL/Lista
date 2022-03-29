1f9bcb2b95855f08b4f1290541fb08ee6dfbd7cf
compuexpressmanzanillo <compuexpressmanzanillo@outlook.com>
2022-03-29 16:11:56 -0600

    second

--- Lista/Program.cs
+++ Lista/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lista
 {
@@ -7,20 +8,21 @@ namespace Lista
     {
         static void Main(string[] args)
         {
-            List<string> Lista3 = new List<string>();
-            List<string> Lista1 = new List<string> { "A", "B", "C", "D", "E", "F" };
-            List<string> Lista2 = new List<string> { "B", "E", "G", "H", "I", "J" };
-
-            
-            for (int i = 0; i < Lista1.Count; i++)
+            List<string> list1 = new List<string> { "A","B","C","D","E","F"};
+            List<string> list2 = new List<string> { "B","E","G","H","I","J" };
+            List<string> list3 = new List<string>();
+            var primera = list1.Where(i => !list2.Contains(i)).ToList();
+            var segunda = list2.Where(i => !list1.Contains(i)).ToList();
+            Console.WriteLine("En Lista1 no en Lista2");
+            foreach (var x in primera)
             {
-                              
-                    if (Lista1[i] == Lista2[i])
-                    {
-                        Lista3.Add(Lista1[i].ToString());
-                        Console.WriteLine(Lista3[i].ToString());
-                    }
-                                
+                list3.Add(x);
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("En Lista2 pero no en Lista1");
+            foreach (var y in segunda)
+            {
+                Console.WriteLine(y);
             }
         }
     }
