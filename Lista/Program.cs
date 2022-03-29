521fc629d4a4183ae820be8c3cdbbdc0c5776aac
compuexpressmanzanillo <compuexpressmanzanillo@outlook.com>
2022-03-29 16:01:32 -0600

    Agregar archivos de proyecto.

---
+++ Lista/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Lista3 = new List<string>();
+            List<string> Lista1 = new List<string> { "A", "B", "C", "D", "E", "F" };
+            List<string> Lista2 = new List<string> { "B", "E", "G", "H", "I", "J" };
+
+            
+            for (int i = 0; i < Lista1.Count; i++)
+            {
+                              
+                    if (Lista1[i] == Lista2[i])
+                    {
+                        Lista3.Add(Lista1[i].ToString());
+                        Console.WriteLine(Lista3[i].ToString());
+                    }
+                                
+            }
+        }
+    }
+}
